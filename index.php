ee17d3c41d74472be35f7cc48b1fceed9b91118f
LE <farzad.vb@gmail.com>
2013-08-05 00:00:29 +0430

    Commit 1 Hello World

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: LE
+ * Date: 8/4/13
+ * Time: 11:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'hi';
+?>
